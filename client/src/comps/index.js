307560f9aa9f6acdb1a2492d08caf17d7f79977b
unknown <vedant.a.rahane@gmail.com>
2021-06-30 21:11:12 +0530

    empty repo

--- client/src/comps/index.js
+++
@@ -1,6 +0,0 @@
-export { default as PostList } from "./PostList";
-export { default as PostCreate } from "./PostCreate";
-export { default as PostEdit } from "./PostEdit";
-export { default as UserList } from "./UserList";
-export { default as UserCreate } from "./UserCreate";
-export { default as UserEdit } from "./UserEdit";
